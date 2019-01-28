3db86238a1318b05a3643ad474c65759cc129e2f
Anatoly Myaskov <myaskov@me.com>
2019-01-28 13:37:24 +0300

    commit

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "DateToolsSwift",
-    targets: [
-        Target(name: "DateToolsSwift")
-    ]
-)
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
